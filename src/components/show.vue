bcfa33278502dcf388bc30ec38ce63fe699d3482
wuyudianye <1723030187@qq.com>
2017-12-11 22:22:22 +0800

    更新模块

--- src/components/show.vue
+++ src/components/show.vue
@@ -24,41 +24,11 @@
           <span class="recommond">推荐主播</span>
           <span class="right">当前<span class="totalNum">5849</span>个主播  ></span>
       </p>
-      <div class="aboutMe" @click="showMe">
-                 <img src="../assets/image/me.png" alt="" class="me">
-                 <img src="../assets/image/next.png" alt="" class="next">              
-      </div>
+   
       <transition name="fade">
             <div class="mask" v-show="isAbout"></div>                       
       </transition>
-      <transition name="slide">
-        <div class="about" v-if="isAbout" >
-            <span class="close" @click="back">X</span>
-            <div class="title">关于</div>
-            <div class="name">游洪莲</div>
-            <div class="job">求职意向: <span class="jobWanted">web前端工程师</span></div>
-            <div > 
-                <p class="job">个人信息: </p>
-                <span class="jobDetail">东华理工大学(本科)</span></br>
-                <span class="jobDetail">软件工程(2018毕业)</span></br>
-                <span class="jobDetail">13257918110</span></br>
-                <span class="jobDetail">994312463@qq.com</span>
-            </div>
-            <div>
-                <p class="skill">技术栈:</p>
-                <span class="skillDetail">
-                    【前端】HTML/HTML5   CSS/CSS3   JavaScript(ES6,ES5)   SASS   Stylus   Vue.JS   AJAX   jQuery   微信小程序   Babel</br>
-                    【后端】Node.js (express)</br>
-                    【数据库】MongoDB</br>
-                    【自动化构建】Webpack</br>
-                    【版本管理】git
-                </span>
-            </div>
-            <div class="desc">个人网站:
-                <a href="http://youhonglian.github.io">Youuu</a>
-            </div>
-        </div>
-      </transition>
+    
       <div class="show-content">
         <div class="showItem col-xs-6" @click="showDetail(item.archive.aid)" v-for="item in showList" key="item.archive.aid">
             <img :src="item.pic" alt="" class="showImg">    
@@ -81,7 +51,6 @@ import vSwiper from '@/components/swiper.vue'
 export default {
     data () {
         return {
-            isAbout:'',
             props: {
                 banner: Object 
              }
@@ -117,12 +86,7 @@ export default {
                  {name: 'showPlay', params: {'aid': aid}}
              )
          },
-        showMe() {
-            this.isAbout = true;
-        },
-        back () {
-            this.isAbout = false
-        },
+       
     }
 
 }
